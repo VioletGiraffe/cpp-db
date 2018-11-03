b2099b3c0eeebb4b413d8751fd1725163559897d
VioletGiraffe <alexxxx89@ya.ru>
2018-11-03 23:24:55 +0200

    WIP

--- src/cpp-db.hpp
+++ src/cpp-db.hpp
@@ -1,2 +1,38 @@
 #pragma once
 
+#include <optional>
+#include <tuple>
+#include <vector>
+
+#define DEFINE_FIELD(Type, name)
+
+enum class TestCollectionFields {
+	Id,
+	Text
+};
+
+template <typename T, auto fieldIdValue>
+struct Field {
+	T value;
+	static constexpr auto id = fieldIdValue;
+};
+
+template <class... Fields>
+class Collection
+{
+public:
+	using Record = std::tuple<Fields...>;
+
+	template <auto N>
+	using FieldTypeById = typename std::tuple_element<static_cast<size_t>(N), Record>::type;
+
+	template <auto fieldTypeEnumValue>
+	void write(const FieldTypeById<fieldTypeEnumValue>& value) {
+
+	}
+
+	template <auto fieldTypeEnumValue>
+	std::vector<Record> find(const FieldTypeById<fieldTypeEnumValue>& queryValue) {
+		return {};
+	}
+};
