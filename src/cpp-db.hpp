edb1d6b1c07e76be75c2bb083a15732dc5550b74
VioletGiraffe <alexxxx89@ya.ru>
2018-12-14 09:15:17 +0200

    WIP

--- src/cpp-db.hpp
+++ src/cpp-db.hpp
@@ -5,6 +5,7 @@
 #include "dbstorage.hpp"
 
 #include "parameter_pack/parameter_pack_helpers.hpp"
+#include "assert/advanced_assert.h"
 
 #include <string>
 #include <tuple>
@@ -23,18 +24,34 @@ class Collection
 		if constexpr (sizeof...(Fields) == 1)
 			return true;
 
-		bool success = true;
-		static_for<1, sizeof...(Fields)>([&success](auto i) {
-			using type0 = pack::type_by_index<decltype(i)::value - 1, Fields...>;
-			using type1 = pack::type_by_index<decltype(i)::value, Fields...>;
+		bool fixedSizeFieldsBeforeDynamicFields = true;
+		static_for<1, sizeof...(Fields)>([&fixedSizeFieldsBeforeDynamicFields](auto i) {
+			using Type0 = pack::type_by_index<decltype(i)::value - 1, Fields...>;
+			using Type1 = pack::type_by_index<decltype(i)::value, Fields...>;
 
-			success = success && !(type0::hasStaticSize() == false && type1::hasStaticSize() == true);
+			fixedSizeFieldsBeforeDynamicFields = fixedSizeFieldsBeforeDynamicFields && !(Type0::hasStaticSize() == false && Type1::hasStaticSize() == true);
 		});
 
-		return success;
+		return fixedSizeFieldsBeforeDynamicFields;
+	}
+
+	static constexpr size_t dynamicFieldCount()
+	{
+		size_t dynamicFieldCount = 0;
+		static_for<0, sizeof...(Fields)>([&dynamicFieldCount](auto i) {
+			using FieldType = pack::type_by_index<decltype(i)::value, Fields...>;
+			if constexpr (FieldType::hasStaticSize() == false)
+				++dynamicFieldCount;
+		});
+
+		return dynamicFieldCount <= 1;
 	}
 
 	static_assert(checkFieldOrder(), "All the fields with static size must be listed before all the fields with runtime size!");
+	static_assert(dynamicFieldCount() <= 1, "No more than one dynamic field is allowed!");
+
+public:
+	using Record = std::tuple<Fields...>;
 
 public:
 	Collection(const std::string& collectionName, const std::string& databaseFolderPath) :
@@ -57,10 +74,9 @@ public:
 		return _dbStoragePath + "/" + _collectionName + "_index/";
 	}
 
-	using Record = std::tuple<Fields...>;
-
 	template <typename Functor, auto... queryFieldIds>
-	void insert(Functor&& predicate, bool updateIfExists = false) {
+	void insert(Functor&& predicate, bool updateIfExists = false)
+	{
 
 	}
 
@@ -74,6 +90,20 @@ public:
 		const auto locations = _index.template find<queryFieldId>(value);
 
 		return results;
+	}
+
+private:
+	Record readRecord(uint64_t recordStartLocation)
+	{
+		assert_r(_storageFile.seek(recordStartLocation));
+		if constexpr (dynamicFieldCount() == 0)
+		{
+
+		}
+		else
+		{
+
+		}
 	}
 
 private:
