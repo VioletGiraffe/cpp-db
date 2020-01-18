2272e3dc8a2e985e43f7dce451de5797a2ac48f1
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-01-18 21:28:57 +0200

    DbFileGaps WIP

--- src/dbfilegaps.hpp
+++ src/dbfilegaps.hpp
@@ -6,28 +6,27 @@
 
 class DbFileGaps
 {
+	struct Gap {
+		uint64_t location;
+		uint64_t length;
+
+		inline constexpr bool operator< (const Gap& other) const {
+			return location < other.location;
+		}
+	};
+
 public:
 	static constexpr auto noGap = std::numeric_limits<uint64_t>::max();
 
-	inline void registerGap(const uint64_t gapOffset) {
-		_gapLocations.insert(gapOffset);
+	inline void registerGap(const uint64_t gapOffset, const uint64_t gapLength) {
+		_gapLocations.emplace(gapOffset, gapLength);
+		// TODO: consolidate gaps
 	}
 
-	inline uint64_t nextGap() const {
-		return _gapLocations.empty() ? noGap : *_gapLocations.begin();
-	}
-
-	inline uint64_t takeNextGap() {
-		const auto begin = _gapLocations.begin();
-		if (begin == _gapLocations.end())
-			return noGap;
-
-		const auto gap = *begin;
-		_gapLocations.erase(begin);
-
-		return gap;
+	inline uint64_t takeSuitableGap(const uint64_t dataLength) {
+		
 	}
 
 private:
-	std::set<uint64_t> _gapLocations;
+	std::set <Gap> _gapLocations;
 };
