a30fb063daa33ad0941bcc588528d847199fce98
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-01-19 20:39:40 +0200

    WIP

--- src/dbfilegaps.hpp
+++ src/dbfilegaps.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "container/std_container_helpers.hpp"
+
 #include <limits>
 #include <set>
 #include <stdint.h>
@@ -9,22 +11,18 @@ class DbFileGaps
 	struct Gap {
 		uint64_t location;
 		uint64_t length;
-
-		inline constexpr bool operator< (const Gap& other) const {
-			return location < other.location;
-		}
 	};
 
 public:
 	static constexpr auto noGap = std::numeric_limits<uint64_t>::max();
 
 	inline void registerGap(const uint64_t gapOffset, const uint64_t gapLength) {
-		_gapLocations.emplace(gapOffset, gapLength);
+		_gapLocations.emplace(Gap{ gapOffset, gapLength });
 		// TODO: consolidate gaps
 	}
 
 	inline uint64_t takeSuitableGap(const uint64_t dataLength) {
-		
+		const auto suitableGapIt = std::find_if(begin_to_end(_gapLocations), [&dataLength](const Gap& gap){return gap.length >= dataLength;});
 	}
 
 private:
