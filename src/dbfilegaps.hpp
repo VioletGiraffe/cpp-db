307949e36d09574f9ace85e414a4ed0d9ecb45b1
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-01-26 20:20:36 +0200

    WIP

--- src/dbfilegaps.hpp
+++ src/dbfilegaps.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "container/std_container_helpers.hpp"
+#include "container/multi_index.hpp"
+#include "assert/advanced_assert.h"
 
 #include <limits>
-#include <set>
 #include <stdint.h>
 
 class DbFileGaps
@@ -11,6 +12,10 @@ class DbFileGaps
 	struct Gap {
 		uint64_t location;
 		uint64_t length;
+		
+		constexpr uint64_t endOffset() const {
+			return location + length;
+		}
 	};
 
 public:
@@ -18,13 +23,43 @@ public:
 
 	inline void registerGap(const uint64_t gapOffset, const uint64_t gapLength) {
 		_gapLocations.emplace(Gap{ gapOffset, gapLength });
-		// TODO: consolidate gaps
 	}
 
-	inline uint64_t takeSuitableGap(const uint64_t dataLength) {
-		const auto suitableGapIt = std::find_if(begin_to_end(_gapLocations), [&dataLength](const Gap& gap){return gap.length >= dataLength;});
+	inline uint64_t takeSuitableGap(const uint64_t gapLength) {
+		const auto [begin, end] = _gapLocations.findSecondaryInRange(gapLength, std::numeric_limits<decltype(Gap::length)>::max());
+		/* TODO: experiment with smart heuristics, e. g.:
+			- taking the shortest suitable gap;
+			- taking the longest suitable gap;
+			- taking the gap with the smallest offset (may or not accidentally be what *begin already refers to).
+			- taking the gap adjacent with another gap for consolidating free space
+		*/
+
+
+		if (begin != end)
+			return (*begin)->location;
+
+		// If no gap is found, consolidate gaps and try again
+		consolidateGaps();
+		const auto [newBegin, newEnd] = _gapLocations.findSecondaryInRange(gapLength, std::numeric_limits<decltype(Gap::length)>::max());
+		return newBegin != newEnd ? (*newBegin)->location : noGap;
 	}
 
 private:
-	std::set <Gap> _gapLocations;
+	void consolidateGaps()
+	{
+		for (auto current = _gapLocations.begin(), next = ++_gapLocations.begin(), end = _gapLocations.end(); next != end; (current = next), ++next)
+		{
+			if (current->endOffset() >= next->location)
+			{
+				assert_debug_only(current->endOffset() == next->location);
+				// Merge current into next
+				// Can't do in-place because std::set's items are const
+				Gap newItem{ current->location, next->endOffset() - current->location };
+				
+			}
+		}
+	}
+
+private:
+	MultiIndexSet<Gap, &Gap::location, &Gap::length> _gapLocations;
 };
