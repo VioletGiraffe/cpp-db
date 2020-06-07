8d23dab057792b60b737049a50c9eba1ecc1a24e
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-06-07 18:11:31 +0300

    WAL WIP

--- src/dbwal.hpp
+++ src/dbwal.hpp
@@ -11,19 +11,19 @@ class DbWAL
 public:
 	[[nodiscard]] bool openLogFile(const std::string& filePath);
 
-	template <class Operation, typename = std::enable_if_t<Operation::op == Operation::Insert::op>>
+	template <class Operation, sfinae<Operation::op == Operation::Insert::op> = true>
 	[[nodiscard]] bool registerOp(const Operation& op);
 
-	template <class Operation, typename = std::enable_if_t<Operation::op == Operation::Find::op>>
+	template <class Operation, sfinae<Operation::op == Operation::Find::op> = true>
 	[[nodiscard]] bool registerOp(const Operation& op);
 
-	template <class Operation, typename = std::enable_if_t<Operation::op == Operation::UpdateFull::op>>
+	template <class Operation, sfinae<Operation::op == Operation::UpdateFull::op> = true>
 	[[nodiscard]] bool registerOp(const Operation& op);
 
-	template <class Operation, typename = std::enable_if_t<Operation::op == Operation::AppendToArray::op>>
+	template <class Operation, sfinae<Operation::op == Operation::AppendToArray::op> = true>
 	[[nodiscard]] bool registerOp(const Operation& op);
 
-	template <class Operation, typename = std::enable_if_t<Operation::op == Operation::Delete::op>>
+	template <class Operation, sfinae<Operation::op == Operation::Delete::op> = true>
 	[[nodiscard]] bool registerOp(const Operation& op);
 
 private:
@@ -34,4 +34,36 @@ template<class Record, class StorageAdapter>
 inline bool DbWAL<Record, StorageAdapter>::openLogFile(const std::string& filePath)
 {
 	return _storage.open(filePath, io::OpenMode::ReadWrite);
+}
+
+template<class Record, class StorageAdapter>
+template<class Operation, sfinae<Operation::op == Operation::Insert::op>>
+inline bool DbWAL<Record, StorageAdapter>::registerOp(const Operation& op)
+{
+	return false;
+}
+
+template<class Record, class StorageAdapter>
+template<class Operation, sfinae<Operation::op == Operation::Find::op>>
+inline bool DbWAL<Record, StorageAdapter>::registerOp(const Operation& op)
+{
+	return false;
+}
+template<class Record, class StorageAdapter>
+template<class Operation, sfinae<Operation::op == Operation::UpdateFull::op>>
+inline bool DbWAL<Record, StorageAdapter>::registerOp(const Operation& op)
+{
+	return false;
+}
+template<class Record, class StorageAdapter>
+template<class Operation, sfinae<Operation::op == Operation::AppendToArray::op>>
+inline bool DbWAL<Record, StorageAdapter>::registerOp(const Operation& op)
+{
+	return false;
+}
+template<class Record, class StorageAdapter>
+template<class Operation, sfinae<Operation::op == Operation::Delete::op>>
+inline bool DbWAL<Record, StorageAdapter>::registerOp(const Operation& op)
+{
+	return false;
 }
