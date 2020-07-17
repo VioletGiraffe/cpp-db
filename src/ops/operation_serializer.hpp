806325857204636bebad985a043cd0b35d81ad9e
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-07-17 23:02:17 +0300

    WIP

--- src/ops/operation_serializer.hpp
+++ src/ops/operation_serializer.hpp
@@ -15,56 +15,56 @@ class Serializer<DbSchema<RecordType>>
 	using Schema = DbSchema<RecordType>;
 
 public:
-	template <class Operation, sfinae<Operation::op == OpCode::Insert> = true>
-	static [[nodiscard]] data_buffer serialize(const Operation& op);
+	template <class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::Insert> = true>
+	static [[nodiscard]] bool serialize(const Operation& op);
 
-	template <class Operation, sfinae<Operation::op == OpCode::Find> = true>
-	static [[nodiscard]] data_buffer serialize(const Operation& op);
+	template <class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::Find> = true>
+	static [[nodiscard]] bool serialize(const Operation& op);
 
-	template <class Operation, sfinae<Operation::op == OpCode::UpdateFull> = true>
-	static [[nodiscard]] data_buffer serialize(const Operation& op);
+	template <class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::UpdateFull> = true>
+	static [[nodiscard]] bool serialize(const Operation& op);
 
-	template <class Operation, sfinae<Operation::op == OpCode::AppendToArray> = true>
-	static [[nodiscard]] data_buffer serialize(const Operation& op);
+	template <class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::AppendToArray> = true>
+	static [[nodiscard]] bool serialize(const Operation& op);
 
-	template <class Operation, sfinae<Operation::op == OpCode::Delete> = true>
-	static [[nodiscard]] data_buffer serialize(const Operation& op);
+	template <class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::Delete> = true>
+	static [[nodiscard]] bool serialize(const Operation& op);
 
 	template <typename Receiver>
 	static bool deserialize(std::span<const char8_t> data, Receiver&& receiver);
 };
 
 template <class RecordType>
-template<class Operation, sfinae<Operation::op == OpCode::Insert>>
-data_buffer Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
+template<class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::Insert>>
+bool Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
 {
 	return false;
 }
 
 template <class RecordType>
-template<class Operation, sfinae<Operation::op == OpCode::Find>>
-data_buffer Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
+template<class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::Find>>
+bool Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
 {
 	return false;
 }
 
 template <class RecordType>
-template<class Operation, sfinae<Operation::op == OpCode::UpdateFull>>
-data_buffer Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
+template<class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::UpdateFull>>
+bool Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
 {
 	return false;
 }
 
 template <class RecordType>
-template<class Operation, sfinae<Operation::op == OpCode::AppendToArray>>
-data_buffer Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
+template<class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::AppendToArray>>
+bool Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
 {
 	return false;
 }
 
 template <class RecordType>
-template<class Operation, sfinae<Operation::op == OpCode::Delete>>
-data_buffer Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
+template<class Operation, typename StorageImplementation, sfinae<Operation::op == OpCode::Delete>>
+bool Serializer<DbSchema<RecordType>>::serialize(const Operation& op)
 {
 	return false;
 }
