9f019053ca748e47e8f426cb62eea59adb265093
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-07-22 01:17:38 +0300

    WIP

--- src/dbstorage.hpp
+++ src/dbstorage.hpp
@@ -45,7 +45,7 @@ public:
 		std::lock_guard locker(_storageMutex);
 
 		assert_and_return_r(_storageFile.seek(recordStartLocation.location), false);
-		return deserializeRecord(record, _storageFile);
+		return RecordSerializer<Record>::deserialize(record, _storageFile);
 	}
 
 	[[nodiscard]] bool writeRecord(const Record& record, const StorageLocation recordStartLocation)
@@ -53,7 +53,7 @@ public:
 		std::lock_guard locker(_storageMutex);
 
 		assert_and_return_r(_storageFile.seek(recordStartLocation.location), false);
-		return serializeRecord(record, _storageFile);
+		return RecordSerializer<Record>::serialize(record, _storageFile);
 	}
 
 private:
