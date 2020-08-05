5d14f0c2b379a6049e2fea3681bf003064417f40
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-08-05 19:00:59 +0300

    Additional test, missing typename added

--- tests/test-app/dbwal_tests.cpp
+++ tests/test-app/dbwal_tests.cpp
@@ -70,37 +70,73 @@ TEST_CASE("Operation::UpdateFull serialization", "[dbops]") {
 	using F_ull = Field<uint64_t, 5>;
 	using Fs = Field<std::string, 42>;
 
-	using Record = DbRecord<Tombstone<F_ull, std::numeric_limits<uint64_t>::max()>, F3, F_ull, Fs>;
+	{
+		using Record = DbRecord<Tombstone<F_ull, std::numeric_limits<uint64_t>::max()>, F3, F_ull, Fs>;
 
-	Operation::Serializer<Record> serializer;
+		Operation::Serializer<Record> serializer;
 
-	using KeyField = Fs;
-	Operation::UpdateFull<Record, KeyField> op{ Record{3.14, 15, "Hello World!"}, "123" };
+		using KeyField = Fs;
+		Operation::UpdateFull<Record, KeyField, false> op{ Record{3.14, 15, "Hello World!"}, "123" };
 
-	StorageIO<io::QMemoryDeviceAdapter> buffer;
-	REQUIRE(buffer.open(".", io::OpenMode::ReadWrite));
+		StorageIO<io::QMemoryDeviceAdapter> buffer;
+		REQUIRE(buffer.open(".", io::OpenMode::ReadWrite));
 
-	REQUIRE(serializer.serialize(op, buffer));
+		REQUIRE(serializer.serialize(op, buffer));
 
-	REQUIRE(buffer.seek(0));
-	const bool success = serializer.deserialize(buffer, [&](auto&& newOp) {
-		using OpType = remove_cv_and_reference_t<decltype(newOp)>;
-		if constexpr (OpType::op == OpCode::UpdateFull)
-		{
-			if constexpr (std::is_same_v<KeyField, OpType::KeyField>)
+		REQUIRE(buffer.seek(0));
+		const bool success = serializer.deserialize(buffer, [&](auto&& newOp) {
+			using OpType = remove_cv_and_reference_t<decltype(newOp)>;
+			if constexpr (OpType::op == OpCode::UpdateFull)
 			{
-				REQUIRE(newOp.insertIfNotPresent == op.insertIfNotPresent);
-				REQUIRE(newOp.record == op.record);
-				REQUIRE(newOp.keyValue == op.keyValue);
+				if constexpr (std::is_same_v<KeyField, typename OpType::KeyField>)
+				{
+					REQUIRE(newOp.insertIfNotPresent == op.insertIfNotPresent);
+					REQUIRE(newOp.record == op.record);
+					REQUIRE(newOp.keyValue == op.keyValue);
+				}
+				else
+					FAIL();
 			}
 			else
 				FAIL();
-		}
-		else
-			FAIL();
-	});
+			});
 
-	REQUIRE(success);
+		REQUIRE(success);
+	}
+
+	{
+		using Record = DbRecord<NoTombstone, F3, Fs, Field<std::string, 45>>;
+
+		Operation::Serializer<Record> serializer;
+
+		using KeyField = F3;
+		Operation::UpdateFull<Record, KeyField, true> op{ Record{3.14, "Hello World!", "I am alive!"}, 5.0 };
+
+		StorageIO<io::QMemoryDeviceAdapter> buffer;
+		REQUIRE(buffer.open(".", io::OpenMode::ReadWrite));
+
+		REQUIRE(serializer.serialize(op, buffer));
+
+		REQUIRE(buffer.seek(0));
+		const bool success = serializer.deserialize(buffer, [&](auto&& newOp) {
+			using OpType = remove_cv_and_reference_t<decltype(newOp)>;
+			if constexpr (OpType::op == OpCode::UpdateFull)
+			{
+				if constexpr (std::is_same_v<KeyField, typename OpType::KeyField>)
+				{
+					REQUIRE(newOp.insertIfNotPresent == op.insertIfNotPresent);
+					REQUIRE(newOp.record == op.record);
+					REQUIRE(newOp.keyValue == op.keyValue);
+				}
+				else
+					FAIL();
+			}
+			else
+				FAIL();
+			});
+
+		REQUIRE(success);
+	}
 }
 
 TEST_CASE("Operation::Delete serialization", "[dbops]") {
@@ -125,7 +161,7 @@ TEST_CASE("Operation::Delete serialization", "[dbops]") {
 		using OpType = remove_cv_and_reference_t<decltype(newOp)>;
 		if constexpr (OpType::op == OpCode::Delete)
 		{
-			if constexpr (std::is_same_v<KeyField, OpType::KeyField>)
+			if constexpr (std::is_same_v<KeyField, typename OpType::KeyField>)
 			{
 				REQUIRE(newOp.keyValue == op.keyValue);
 			}
