35e3d6dd4c69bbef81d6e03076b699be61ca1419
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2022-12-24 13:54:41 +0200

    DbWAL tests WIP

--- tests/test-app/dbwal_tests.cpp
+++ tests/test-app/dbwal_tests.cpp
@@ -66,3 +66,39 @@ TEST_CASE("DbWAL basics and normal operation", "[dbwal]")
 
 	REQUIRE(wal.closeLogFile());
 }
+
+/* Entry structure :
+
+   -------------------------------------------------------------------------------------------------
+  |            Field                |           Size              |        Offset from start        |
+  |---------------------------------|-----------------------------|---------------------------------|
+  | Complete entry SIZE (inc. hash) | sizeof(EntrySizeType) bytes |                         0 bytes |
+  | Operation STATUS                |                     1 byte  | sizeof(EntrySizeType)     bytes |
+  | Serialized operation DATA       |         var. length         | sizeof(EntrySizeType) + 1 bytes |
+  | HASH of the whole entry data    |      sizeof(HashType) bytes | [SIZE] - sizeof(HashType) bytes |
+   -------------------------------------------------------------------------------------------------
+
+   NOTE: hash is always calculated with STATUS == PENDING. It's not updated when the final status is patched in.
+
+*/
+
+//TEST_CASE("DbWAL - verifying storage format", "[dbwal]")
+//{
+//	using F64 = Field<uint64_t, 1>;
+//	using F16 = Field<int16_t, 2>;
+//
+//	using FArray = Field<uint32_t, 3, true>;
+//	using RecordWithArray = DbRecord<F16, FArray>;
+//
+//	DbWAL<RecordWithArray, io::StaticBufferAdapter<4096>> wal;
+//	REQUIRE(wal.openLogFile({}));
+//	REQUIRE(wal.clearLog());
+//
+//	Operation::AppendToArray<RecordWithArray, F16, FArray, false> opAppend(uint16_t{ 0 }, std::vector{ uint32_t{42} });
+//	constexpr auto opID = 0xFFFFFFFF123456;
+//	REQUIRE(wal.registerOperation(opID, opAppend));
+//
+//	REQUIRE(wal.closeLogFile());
+//
+//	wal.
+//}
